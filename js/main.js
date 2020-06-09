567059e58e9d1ef8da2b2fdcf54022f2d889624b
katareena <w10160177@gmail.com>
2020-06-09 16:45:26 +0300

    исправления

--- js/main.js
+++ js/main.js
@@ -2,23 +2,35 @@
 
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS_OBJECTS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var TIMES = ['12:00', '13:00', '14:00'];
 var TYPES_OBJECT = ['palace', 'flat', 'house', 'bungalo'];
 var TITLES = ['Уникальные Бизнес апартаменты в стиле Лофт', 'Большая светлая квартира', 'Просторная студия', 'Двухуровневые апартаменты', 'Квартира в историческом центре'];
 var DISCRIPTIONS = ['description1', 'description2', 'description3', 'description4', 'description5', 'description6', 'description7', 'description8'];
-var ROOMS = [1, 2, 3, 4];
-var GUESTS = [1, 2, 3, 4, 5, 6, 7, 8];
+var TIMES = ['12:00', '13:00', '14:00'];
+var PIN_HALF_WIDTH = 25;
+var PIN_HEIGHT = 70;
+var NUMBER_OF_ADS = 8;
 
-function getRandom(lower, upper) {
+var getRandom = function (lower, upper) {
   var min = Math.ceil(lower);
   var max = Math.floor(upper);
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
+
+var getRandomАrray = function (ceil, floor, name) {
+  var length = getRandom(ceil, floor);
+  var arrayNew = name.slice();
+
+  while (arrayNew.length > length) {
+    arrayNew.splice(getRandom(ceil, arrayNew.length), 1);
+  }
+
+  return arrayNew;
+};
 
 var createOneAd = function (i) {
   var X = getRandom(1, 1200);
   var Y = getRandom(130, 630);
-  var PRICE = getRandom(200, 1500);
+
   var template = {
     author: {
       avatar: 'img/avatars/user' + '0' + (i + 1) + '.png',
@@ -26,15 +38,15 @@ var createOneAd = function (i) {
     offer: {
       title: TITLES [i % TITLES.length],
       address: X + ', ' + Y,
-      price: PRICE,
+      price: getRandom(200, 1500),
       type: TYPES_OBJECT[i % TYPES_OBJECT.length],
-      rooms: ROOMS[i % ROOMS.length],
-      guests: GUESTS[i],
+      rooms: getRandom(1, 4),
+      guests: getRandom(1, 6),
       checkin: TIMES[i % TIMES.length],
       checkout: TIMES[i % TIMES.length],
-      features: FEATURES[i % FEATURES.length],
+      features: getRandomАrray(0, 5, FEATURES),
       description: DISCRIPTIONS[i % DISCRIPTIONS.length],
-      photos: PHOTOS_OBJECTS[i % PHOTOS_OBJECTS.length],
+      photos: getRandomАrray(0, 2, PHOTOS_OBJECTS),
     },
     location: {
       x: X,
@@ -46,31 +58,15 @@ var createOneAd = function (i) {
 };
 
 var createAds = function () {
-  var card = [];
-  for (var i = 0; i < 8; i++) {
-    card.push(createOneAd(i));
+  var cards = [];
+  for (var i = 0; i < NUMBER_OF_ADS; i++) {
+    cards.push(createOneAd(i));
   }
-  return card;
+
+  return cards;
 };
 
-var ADS = createAds();
-
-// var similarAdTemplate = document.querySelector('#card')
-//   .content
-//   .querySelector('.popup');
-
-// var renderAd = function (parametrs) {
-//   var adElement = similarAdTemplate.cloneNode(true);
-//   adElement.querySelector('.popup__avatar').src = parametrs.author.avatar;
-
-//   return adElement;
-// };
-
-// var fragment = document.createDocumentFragment();
-// for (var i = 0; i < ADS.length; i++) {
-//   fragment.appendChild(renderAd(ADS[i]));
-// }
-// console.log(fragment);
+var ads = createAds();
 
 // ------------------------ 2 -----------------------
 
@@ -81,16 +77,16 @@ var similarPinTemplate = document.querySelector('#pin').content.querySelector('.
 
 var renderPin = function (parametrs) {
   var pinElement = similarPinTemplate.cloneNode(true);
-  pinElement.style.left = parametrs.location.x - 25 + 'px';
-  pinElement.style.top = parametrs.location.y - 70 + 'px';
+  pinElement.style.left = parametrs.location.x - PIN_HALF_WIDTH + 'px';
+  pinElement.style.top = parametrs.location.y - PIN_HEIGHT + 'px';
   pinElement.children[0].src = parametrs.author.avatar;
   pinElement.children[0].alt = parametrs.offer.title;
   return pinElement;
 };
 
 var fragmentPins = document.createDocumentFragment();
-for (var i = 0; i < ADS.length; i++) {
-  fragmentPins.appendChild(renderPin(ADS[i]));
+for (var i = 0; i < ads.length; i++) {
+  fragmentPins.appendChild(renderPin(ads[i]));
 }
 
 var map = document.querySelector('.map');
